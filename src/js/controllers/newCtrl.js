f865d2dc433abbfd3dc12960d0d9a446da8af748
Matt Kraatz <matt.kraatz@gmail.com>
2016-09-14 11:20:45 -0500

    cleaned up groupmeFact.getMessages to work in a modularized state

--- src/js/controllers/newCtrl.js
+++ src/js/controllers/newCtrl.js
@@ -1,6 +1,33 @@
 "use strict";
 
-app.controller('newCtrl',function($scope) {
+app.controller('newCtrl',function($scope,groupmeFact) {
+
+  $scope.getConversations = () => {
+    if ($scope.$parent.userAccessToken) {
+      groupmeFact.getGroupList($scope.$parent.userAccessToken)
+        .then((groupArray) => {
+          $scope.groupOptions = groupArray;
+        })
+    } else {
+      firebase.auth().onAuthStateChanged((user) => {
+        groupmeFact.getGroupList($scope.$parent.userAccessToken)
+          .then((groupArray) => {
+            $scope.groupOptions = groupArray;
+          })
+      })
+    }
+  }
+
+  $scope.groupSelect = '';
+
+  $scope.startingMessageID = '';
+
+  $scope.getMessages = () => {
+    groupmeFact.getMessages($scope.groupSelect.group_id,$scope.$parent.userAccessToken,$scope.startingMessageID)
+      .then((msgList) => {
+        console.log(msgList)
+      })
+  }
 
   // TurnJS Configuration
   $scope.readyFlipbook = () => {
--- src/js/factories/groupmeFact.js
+++ src/js/factories/groupmeFact.js
@@ -2,14 +2,16 @@
 
 app.factory('groupmeFact', function($q,$http){
 
-  let returnLimit = 100;
+  let messageReturnLimit = 100,
+      groupReturnLimit = 50,
+      queriedMessages = 0;
 
   let getGroupList = (accessToken) => {
     return $q((resolve,reject) => {
-      $http.get("https://api.groupme.com/v3/groups?token=" + accessToken)
+      $http.get("https://api.groupme.com/v3/groups?token=" + accessToken + "&per_page=" + groupReturnLimit)
         .then((data) => {
-            console.log("return from firebase", data);
-            return data.response;
+            console.log("return from firebase", data.data.response);
+            resolve(data.data.response);
           }, (error) => {
             console.error(error);
             reject(error);
@@ -19,39 +21,79 @@ app.factory('groupmeFact', function($q,$http){
   };
 
   // Grab first set of messages for selected group
-  let getMessages = (groupID,accessToken) => {
-    $.getJSON("https://api.groupme.com/v3/groups/" + groupID + "/messages?token=" + accessToken + "&limit=" + returnLimit)
-    .then((d) => {
-      let msg = d.response.messages;
-      msg.forEach(function(m){messageList.push(m);});
-      messagesLength = d.response.count;
-      if ($("#message-id")[0].value === "") {
-        lastMessageID = msg[(msg.length - 1)].id;
-      } else {
-        lastMessageID = $("#message-id")[0].value;
-      }
-      loopMessages(lastMessageID,messagesLength,groupID);
+  let getMessages = (groupID,accessToken,startingMessageID) => {
+    return $q((resolve,reject) => {
+      $http.get("https://api.groupme.com/v3/groups/" + groupID + "/messages?token=" + accessToken + "&limit=" + messageReturnLimit)
+        .then((data) => {
+          let msgList = data.data.response.messages;
+          let conversationLength = data.data.response.count;
+          if (msgList.length === 0) {
+            resolve(msgList)
+          } else {
+            let lastMessageID = msgList[(msgList.length - 1)].id;
+            // Pull additional messages as many times as necessary
+            let loopMessages = () => {
+              $http.get("https://api.groupme.com/v3/groups/" + groupID + "/messages?token=" + accessToken + "&limit=" + messageReturnLimit + "&before_id=" + lastMessageID)
+                .then((data) => {
+                  let messages = data.data.response.messages;
+                  messages.forEach(function(msg){msgList.push(msg);});
+                  lastMessageID = msgList[(msgList.length - 1)].id;
+                  checkMessages();
+                })
+              }
+            // Check for number of retrieved messages against the length of the conversation
+            let checkMessages = () => {
+              queriedMessages = queriedMessages + messageReturnLimit || messageReturnLimit;
+              if (queriedMessages <= conversationLength) {
+                loopMessages();
+              } else {
+                console.log(msgList);
+                resolve(msgList)
+            }}
+            checkMessages();
+          }
+        }, (error) => {
+          console.error(error);
+          reject(error);
+        })
     })
-    .catch(function(error){console.error(error);});
-  };
+  }
 
   // Loops through remaining messages to display all
-  let loopMessages = (lastMessageID,messagesLength,groupID) => {
-    queriedMessages = queriedMessages + returnLimit || returnLimit;
-    if (queriedMessages <= messagesLength) {
-      $.getJSON("https://api.groupme.com/v3/groups/" + groupID + "/messages?token=" + $("#access-token")[0].value + "&limit=" + returnLimit + "&before_id=" + lastMessageID)
-      .then((d) => {
-        let msg = d.response.messages;
-        msg.forEach(function(m){messageList.push(m);});
-        lastMessageID = msg[(msg.length - 1)].id;
-        loopMessages(lastMessageID,messagesLength,groupID);
-      })
-      .catch(function(error){console.error(error);});
-    } else {
-      printMessages(messageList);
-      $("#flipbook").turn("page", 1).turn("stop");
-    }
-  };
+  // let loopMessages = (msgList,accessToken,lastMessageID,conversationLength,groupID) => {
+  //   $q((resolve,reject) => {
+  //     queriedMessages = queriedMessages + messageReturnLimit || messageReturnLimit;
+  //     if (queriedMessages <= conversationLength) {
+  //       $http.get("https://api.groupme.com/v3/groups/" + groupID + "/messages?token=" + accessToken + "&limit=" + messageReturnLimit + "&before_id=" + lastMessageID)
+  //       .then((data) => {
+  //         let messages = data.data.response.messages;
+  //         messages.forEach(function(msg){msgList.push(msg);});
+  //         lastMessageID = msgList[(msgList.length - 1)].id;
+  //         loopMessages(msgList,lastMessageID,conversationLength,groupID);
+  //       })
+  //       .catch(function(error){console.error(error);});
+  //     } else {
+  //       resolve(msgList)
+        // printMessages(messageList);
+        // $("#flipbook").turn("page", 1).turn("stop");
+  //     }
+  //   })
+  // };
+  //   $.getJSON("https://api.groupme.com/v3/groups/" + groupID + "/messages?token=" + accessToken + "&limit=" + messageReturnLimit)
+  //   .then((d) => {
+  //     let msg = d.response.messages;
+  //     msg.forEach(function(m){messageList.push(m);});
+  //     messagesLength = d.response.count;
+  //     if ($("#message-id")[0].value === "") {
+  //       lastMessageID = msg[(msg.length - 1)].id;
+  //     } else {
+  //       lastMessageID = $("#message-id")[0].value;
+  //     }
+  //     loopMessages(lastMessageID,messagesLength,groupID);
+  //   })
+  //   .catch(function(error){console.error(error);});
+  // };
+
 
   return {getGroupList,getMessages}
 
