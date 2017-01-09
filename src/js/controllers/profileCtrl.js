2973e9477efa781c60fcb0f2bc69d002cdeac19a
Matt Kraatz <matt.kraatz@gmail.com>
2017-01-09 13:12:27 -0600

    cleaned up working directory

--- src/js/controllers/profileCtrl.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-app.controller('profileCtrl',function($scope) {
-
-  $scope.updateToken = () => {
-    let accessToken = $scope.$parent.userAccessToken;
-    firebase.database().ref(`users/${$scope.$parent.currentUser}/accessToken`).set(accessToken)
-  };
-});
